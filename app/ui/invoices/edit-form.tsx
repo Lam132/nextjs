5a80807a12a11da4961752d9bc2a22906534b80a
Lam132 <lamdz1311@gmail.com>
2023-11-19 01:34:11 +0700

    nextjs

---
+++ app/ui/invoices/edit-form.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { updateInvoice } from '@/app/lib/actions';
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import {
+  CheckIcon,
+  ClockIcon,
+  CurrencyDollarIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { Button } from '@/app/ui/button';
+
+export default function EditInvoiceForm({
+  invoice,
+  customers,
+}: {
+  invoice: InvoiceForm;
+  customers: CustomerField[];
+}) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+ 
+  return (
+    <form action={updateInvoiceWithId}>
+      <input type="hidden" name="id" value={invoice.id} />
+    </form>
+  );}
